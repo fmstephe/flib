800483e25cc2f93b7ca5bde254e95bee509d2341
Francis Stephens <francisstephens@gmail.com>
2014-09-04 18:18:19 +0100

    Added unit tests for spscq.commonQ
    commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() advances write field
    
    commonQ.AcquireRead(...)/commonQ.ReleaseRead() advances read field
    
    commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow write to
    wrap past read
    
    commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow read to
    overtake write

--- queues/spscq/tcommon_test.go
+++ queues/spscq/tcommon_test.go
@@ -1,18 +1,23 @@
 package spscq
 
 import (
-	"testing"
 	"github.com/fmstephe/flib/fmath"
+	"math/rand"
+	"testing"
 )
 
+const maxSize = 1 << 42 // Represents a very big ring buffer
+
+// Test that we can call newCommonQ(...) for every power of 2 in an int64
 func TestNewCommonQPowerOf2(t *testing.T) {
 	for size := int64(1); size > 0; size *= 2 {
 		newCommonQ(size)
 	}
 }
 
+// Test that we can't call newCommonQ(...) with a non-power of 2 size
 func TestNewCommonQNotPowerOf2(t *testing.T) {
-	for size := int64(1); size < 10 * 1000; size++ {
+	for size := int64(1); size < 10*1000; size++ {
 		if !fmath.PowerOfTwo(size) {
 			makeBadQ(size, t)
 		}
@@ -26,4 +31,84 @@ func makeBadQ(size int64, t *testing.T) {
 		}
 	}(size)
 	newCommonQ(size)
+}
+
+// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() advances write field
+func TestAcquireReleaseWriteAdvancesWriteField(t *testing.T) {
+	rand.Seed(1)
+	for size := int64(1); size <= maxSize; size *= 2 {
+		rem := size
+		cq := newCommonQ(size)
+		for rem > 0 {
+			bufferSize := rand.Int63n(rem) + 1
+			cq.acquireWrite(bufferSize)
+			cq.ReleaseWrite()
+			rem -= bufferSize
+			if cq.write.Value != size-rem {
+				t.Errorf("Expecting cq.write == %d, found %d", size-rem, cq.write.Value)
+			}
+		}
+	}
+}
+
+// commonQ.AcquireRead(...)/commonQ.ReleaseRead() advances read field
+func TestAcquireReleaseReadAdvancesReadField(t *testing.T) {
+	rand.Seed(1)
+	for size := int64(1); size <= maxSize; size *= 2 {
+		rem := size
+		cq := newCommonQ(size)
+		cq.acquireWrite(size)
+		cq.ReleaseWrite()
+		for rem > 0 {
+			bufferSize := rand.Int63n(rem) + 1
+			cq.acquireRead(bufferSize)
+			cq.ReleaseRead()
+			rem -= bufferSize
+			if cq.read.Value != size-rem {
+				t.Errorf("Expecting cq.read == %d, found %d", size-rem, cq.read.Value)
+			}
+		}
+	}
+}
+
+// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow write to wrap past read
+func TestAcquireWriteDoesNotWrapPastRead(t *testing.T) {
+	rand.Seed(1)
+	for size := int64(1); size <= maxSize; size *= 2 {
+		writeRand := (size / 64) + 1
+		readRand := (size / 128) + 1
+		cq := newCommonQ(size)
+		for i := 0; i < 10*1000; i++ {
+			writeSize := rand.Int63n(writeRand) + 2
+			readSize := rand.Int63n(readRand) + 1
+			cq.acquireWrite(writeSize)
+			cq.ReleaseWrite()
+			cq.acquireRead(readSize)
+			cq.ReleaseRead()
+			if cq.write.Value-size > cq.read.Value {
+				t.Errorf("Write has overlapped read. cq.write = %d, cq.read = %d, cq.size = %d", cq.write.Value, cq.read.Value, cq.size)
+			}
+		}
+	}
+}
+
+// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow read to overtake write
+func TestAcquireReadDoesNotOvertakeWrite(t *testing.T) {
+	rand.Seed(1)
+	for size := int64(1); size <= maxSize; size *= 2 {
+		writeRand := (size / 128) + 1
+		readRand := (size / 64) + 1
+		cq := newCommonQ(size)
+		for i := 0; i < 10*1000; i++ {
+			writeSize := rand.Int63n(writeRand) + 2
+			readSize := rand.Int63n(readRand) + 1
+			cq.acquireWrite(writeSize)
+			cq.ReleaseWrite()
+			cq.acquireRead(readSize)
+			cq.ReleaseRead()
+			if cq.read.Value > cq.write.Value {
+				t.Errorf("Read has overtaken Write. cq.read = %d, cq.write = %d, cq.size = %d", cq.read.Value, cq.write.Value, cq.size)
+			}
+		}
+	}
 }
