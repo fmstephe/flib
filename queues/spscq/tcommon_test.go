de21540a839a45d0934c051de717181654431590
Francis Stephens <francisstephens@gmail.com>
2014-09-16 16:13:17 +0100

    Replaced commonQ tests with invariant tests
    Previously we had a number of tests ensuring that specific properties
    are maintained for a particular field, such as write or read. These have
    been replaced with a collection of tests which compare the state change
    that occurred with a method call.
    
    Currently we have state change tests for the
    AcquireWrite/ReleaseWrite/Lazy and AcquireRead/ReleaseRead/Lazy methods.

--- queues/spscq/tcommon_test.go
+++ queues/spscq/tcommon_test.go
@@ -1,6 +1,8 @@
 package spscq
 
 import (
+	"errors"
+	"fmt"
 	"github.com/fmstephe/flib/fmath"
 	"math/rand"
 	"testing"
@@ -32,81 +34,136 @@ func makeBadQ(size int64, t *testing.T) {
 	}
 }
 
-// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() advances write field
-func TestAcquireReleaseWriteAdvancesWriteField(t *testing.T) {
-	rand.Seed(1)
-	for size := int64(1); size <= maxSize; size *= 2 {
-		rem := size
-		cq, _ := newCommonQ(size)
-		for rem > 0 {
-			bufferSize := rand.Int63n(rem) + 1
-			cq.acquireWrite(bufferSize)
-			cq.ReleaseWrite()
-			rem -= bufferSize
-			if cq.write.Value != size-rem {
-				t.Errorf("Expecting cq.write == %d, found %d", size-rem, cq.write.Value)
-			}
-		}
+func testAcquireWrite(writeBufferSize, from, to int64, cq, snap commonQ) error {
+	actualWriteSize := to - from
+	if actualWriteSize == 0 && cq.failedWrites.Value != snap.failedWrites.Value+1 {
+		msg := "failedWrites not incremented. Expected %d, found %d"
+		return errors.New(fmt.Sprintf(msg, snap.failedWrites.Value+1, cq.failedWrites.Value))
 	}
+	if actualWriteSize > writeBufferSize {
+		msg := "Actual write size (%d) larger than requested buffer size (%d)"
+		return errors.New(fmt.Sprintf(msg, actualWriteSize, writeBufferSize))
+	}
+	if (actualWriteSize < writeBufferSize) && (cq.write.Value+actualWriteSize) != (cq.read.Value+cq.size) {
+		msg := "Actual write size (%d) could have been bigger.\nsnap %s\ncq  %s"
+		return errors.New(fmt.Sprintf(msg, actualWriteSize, snap.String(), cq.String()))
+	}
+	if (cq.write.Value + actualWriteSize) > (cq.read.Value + cq.size) {
+		msg := "Actual write size (%d) overwrites unread data.\ncq %s"
+		return errors.New(fmt.Sprintf(msg, actualWriteSize, cq.String()))
+	}
+	if cq.writeSize.Value != actualWriteSize {
+		msg := "cq.writeSize does not equal actual write size"
+		return errors.New(fmt.Sprintf(msg, cq.writeSize, actualWriteSize))
+	}
+	if from > to {
+		msg := "from (%d) is greater than to (%d)"
+		return errors.New(fmt.Sprintf(msg, from, to))
+	}
+	if from >= cq.size || from < 0 {
+		msg := "from (%d) must be a valid index for an array of size %d"
+		return errors.New(fmt.Sprintf(msg, from, cq.size))
+	}
+	if to > cq.size || to < 0 {
+		msg := "to (%d) must be a valid index for an array of size %d"
+		return errors.New(fmt.Sprintf(msg, to, cq.size))
+	}
+	return nil
 }
 
-// commonQ.AcquireRead(...)/commonQ.ReleaseRead() advances read field
-func TestAcquireReleaseReadAdvancesReadField(t *testing.T) {
-	rand.Seed(1)
-	for size := int64(1); size <= maxSize; size *= 2 {
-		rem := size
-		cq, _ := newCommonQ(size)
-		cq.acquireWrite(size)
-		cq.ReleaseWrite()
-		for rem > 0 {
-			bufferSize := rand.Int63n(rem) + 1
-			cq.acquireRead(bufferSize)
-			cq.ReleaseRead()
-			rem -= bufferSize
-			if cq.read.Value != size-rem {
-				t.Errorf("Expecting cq.read == %d, found %d", size-rem, cq.read.Value)
-			}
-		}
+func testReleaseWrite(cq, snap commonQ) error {
+	if cq.writeSize.Value != 0 {
+		return errors.New(fmt.Sprintf("cq.writeSize was not reset to 0, %d found instead", cq.writeSize))
 	}
+	if cq.write.Value != snap.write.Value+snap.writeSize.Value {
+		return errors.New(fmt.Sprintf("write has not been advanced by the correct amount.\nsnap %s\ncq  %s", snap.String(), cq.String()))
+	}
+	return nil
 }
 
-// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow write to wrap past read
-func TestAcquireWriteDoesNotWrapPastRead(t *testing.T) {
-	rand.Seed(1)
-	for size := int64(1); size <= maxSize; size *= 2 {
-		writeRand := (size / 64) + 1
-		readRand := (size / 128) + 1
-		cq, _ := newCommonQ(size)
-		for i := 0; i < 10*1000; i++ {
-			writeSize := rand.Int63n(writeRand) + 2
-			readSize := rand.Int63n(readRand) + 1
-			cq.acquireWrite(writeSize)
-			cq.ReleaseWrite()
-			cq.acquireRead(readSize)
-			cq.ReleaseRead()
-			if cq.write.Value-size > cq.read.Value {
-				t.Errorf("Write has overlapped read. cq.write = %d, cq.read = %d, cq.size = %d", cq.write.Value, cq.read.Value, cq.size)
-			}
-		}
+func testAcquireRead(readBufferSize, from, to int64, cq, snap commonQ) error {
+	actualReadSize := to - from
+	if actualReadSize == 0 && cq.failedReads.Value != snap.failedReads.Value+1 {
+		msg := "failedReads not incremented. Expected %d, found %d"
+		return errors.New(fmt.Sprintf(msg, snap.failedReads.Value+1, cq.failedReads.Value))
 	}
+	if actualReadSize > readBufferSize {
+		msg := "Actual read size (%d) larger than requested buffer size (%d)"
+		return errors.New(fmt.Sprintf(msg, actualReadSize, readBufferSize))
+	}
+	if (actualReadSize < readBufferSize) && (cq.read.Value+actualReadSize) != (cq.write.Value) {
+		msg := "Actual read size (%d) could have been bigger.\nsnap %s\ncq  %s"
+		return errors.New(fmt.Sprintf(msg, actualReadSize, snap.String(), cq.String()))
+	}
+	if (cq.read.Value + actualReadSize) > cq.write.Value {
+		msg := "Actual read size (%d) reads past write position (%d).\ncq %s"
+		return errors.New(fmt.Sprintf(msg, actualReadSize, cq.write.Value, cq.String()))
+	}
+	if cq.readSize.Value != actualReadSize {
+		msg := "cq.readSize does not equal actual read size"
+		return errors.New(fmt.Sprintf(msg, cq.readSize, actualReadSize))
+	}
+	if from > to {
+		msg := "from (%d) is greater than to (%d)"
+		return errors.New(fmt.Sprintf(msg, from, to))
+	}
+	if from >= cq.size || from < 0 {
+		msg := "from (%d) must be a valid index for an array of size %d"
+		return errors.New(fmt.Sprintf(msg, from, cq.size))
+	}
+	if to > cq.size || to < 0 {
+		msg := "to (%d) must be a valid index for an array of size %d"
+		return errors.New(fmt.Sprintf(msg, to, cq.size))
+	}
+	return nil
 }
 
-// commonQ.AcquireWrite(...)/commonQ.ReleaseWrite() does not allow read to overtake write
-func TestAcquireReadDoesNotOvertakeWrite(t *testing.T) {
+func testReleaseRead(cq, snap commonQ) error {
+	if cq.readSize.Value != 0 {
+		return errors.New(fmt.Sprintf("cq.readSize was not reset to 0, %d found instead", cq.readSize))
+	}
+	if cq.read.Value != snap.read.Value+snap.readSize.Value {
+		return errors.New(fmt.Sprintf("read has not been advanced by the correct amount.\nsnap %s\ncq   %s", snap.String(), cq.String()))
+	}
+	return nil
+}
+
+func TestEvenReadWrites(t *testing.T) {
 	rand.Seed(1)
-	for size := int64(1); size <= maxSize; size *= 2 {
-		writeRand := (size / 128) + 1
-		readRand := (size / 64) + 1
-		cq, _ := newCommonQ(size)
-		for i := 0; i < 10*1000; i++ {
-			writeSize := rand.Int63n(writeRand) + 2
-			readSize := rand.Int63n(readRand) + 1
-			cq.acquireWrite(writeSize)
+	for i := uint64(0); i <= 41; i++ {
+		size := int64(1 << i)
+		cq, err := newCommonQ(size)
+		if err != nil {
+			t.Error(err.Error())
+			continue
+		}
+		bufferSize := fmath.Max(size/128, 1)
+		for j := int64(0); j < 1024; j++ {
+			// write
+			snap := cq
+			wfrom, wto := cq.acquireWrite(bufferSize)
+			if err := testAcquireWrite(bufferSize, wfrom, wto, cq, snap); err != nil {
+				t.Error(err.Error())
+				return
+			}
+			snap = cq
 			cq.ReleaseWrite()
-			cq.acquireRead(readSize)
+			if err := testReleaseWrite(cq, snap); err != nil {
+				t.Error(err.Error())
+				return
+			}
+			// read
+			snap = cq
+			rfrom, rto := cq.acquireRead(bufferSize)
+			if err := testAcquireRead(bufferSize, rfrom, rto, cq, snap); err != nil {
+				t.Error(err.Error())
+				return
+			}
+			snap = cq
 			cq.ReleaseRead()
-			if cq.read.Value > cq.write.Value {
-				t.Errorf("Read has overtaken Write. cq.read = %d, cq.write = %d, cq.size = %d", cq.read.Value, cq.write.Value, cq.size)
+			if err := testReleaseRead(cq, snap); err != nil {
+				t.Error(err.Error())
+				return
 			}
 		}
 	}
