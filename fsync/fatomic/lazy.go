a2c4bc505dbb914728f98be77838454abbd7b4fb
Francis Stephens <francisstephens@gmail.com>
2015-03-15 04:49:44 +0000

    ASM for LazyStore replaced with Go code
    You can see the trick used to implement this in runtime/atomic_amd64x.go
    (which should appear in Go 1.5). The use of a a nop() function prevents
    the surrounding function from being inlined by the compiler. Because the
    functions are not inlined they cannot be reorded. Just what the doctor
    ordered for LazyStore :)

--- fsync/fatomic/lazy.go
+++ fsync/fatomic/lazy.go
@@ -1,3 +1,15 @@
+// +build go1.4 go1.5
+
 package fatomic
 
-func LazyStore(addr *int64, val int64)
+//go:nosplit
+func LazyStore(addr *int64, val int64) {
+	nop()
+	*addr = val
+}
+
+// Sacrificial inline method
+// prevents inlining of methods will call it
+// This, in turn, prevents reordering
+// See runtime/atomic_amd64x.go
+func nop() {}
