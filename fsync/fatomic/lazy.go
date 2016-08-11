bbd62fe96fdc9f0ea030a871044549f280e807eb
Francis Stephens <francisstephens@gmail.com>
2016-08-11 19:50:56 +0000

    Adding BSD License
     #1
    
    BSD was chosen because it appears to allow for free and extensive use by
    other people while preserving a record of authorship.
    
    I'm not totally sure what the impact of this will be in time. This may
    change.

--- fsync/fatomic/lazy.go
+++ fsync/fatomic/lazy.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Francis Stephens. All rights reserved.
+// Use of this source code is governed by a BSD
+// license which can be found in LICENSE.txt
+
 // +build go1.6
 // +build amd64
 
--- queues/spscq/perf_spscq/pqrw.go
+++ queues/spscq/perf_spscq/pqrw.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Francis Stephens. All rights reserved.
+// Use of this source code is governed by a BSD
+// license which can be found in LICENSE.txt
+
 package main
 
 import (
